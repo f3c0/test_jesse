01e3412bc5cf551a677d9896911348d1206967e1
Ferenc Böröczki <f3c0@users.noreply.github.com>
2018-09-26 17:00:35 +0200

    use updated jesse

--- src/test_jesse_use_it.erl
+++ src/test_jesse_use_it.erl
@@ -4,7 +4,11 @@
 
 use_it() ->
   case jesse:validate_with_schema(<<"x1">>, <<"x2">>,
-    [{parser_fun, fun(_) -> throw(xxx) end}]) of
-    {error,{schema_error,{parse_error,xxx}}} -> error1;
+    [{parser_fun, fun
+                    (<<"x1">>) -> #{};
+                    (_) -> throw(xxx)
+                  end}]) of
+    {error, [{schema_error, {parse_error, xxx}}]} -> error1;
+    {error, [{data_error, {parse_error, xxx}}]} -> error2;
     Any -> io:format("O: ~p~n", [Any])
   end.
